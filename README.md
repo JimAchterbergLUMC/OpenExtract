7e764c657502ceb0200d770a6f3edd37ba58ada4
JimAchterbergLUMC <j.l.achterberg@lumc.nl>
2025-09-01 16:22:45 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ReviewAnalyticsDHA
 Literature review on data analytics for digital health applications.
+
+
+# Usage
+
+- pip install -r requirements.txt
+- Put papers in pdf format in papers/ directory
+- put an OpenRouter API key in some .txt file
+- run main_dense.py with arguments as denoted by argument parser in the script.
--- pretty_print.py
+++ pretty_print.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import json
 import os
+from sklearn.preprocessing import LabelEncoder
 
 df = pd.DataFrame()
-for paper in os.listdir("answers"):
+dir_name = "answers_free_text"
+for paper in os.listdir(dir_name):
     if not paper.endswith(".json"):
         continue
-    results = json.load(open(f"answers/{paper}"))
+    results = json.load(open(f"{dir_name}/{paper}"))
     answers = results["answers"]  # list of dicts
     answers_df = pd.DataFrame(answers)
     answers_df["paper"] = results["paper"]
     df = pd.concat([df, answers_df])
-
+df["paper_id"] = LabelEncoder().fit_transform(df["paper"])
+os.makedirs("answers", exist_ok=True)
 df.to_csv("answers/all_answers.csv", index=False)
